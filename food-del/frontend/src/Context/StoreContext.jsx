c19519e3ba2dc4171626120d24c782346e3304d6
JKM203 <159906836+JKM203@users.noreply.github.com>
2024-09-18 14:48:24 +0530

    Update StoreContext.jsx

--- food-del/frontend/src/Context/StoreContext.jsx
+++ food-del/frontend/src/Context/StoreContext.jsx
@@ -4,8 +4,8 @@ import axios from "axios";
 export const StoreContext = createContext(null);
 
 const StoreContextProvider = (props) => {
-
-    const url = "http://localhost:4000"
+    //http://localhost:4000
+    const url = "https://food-backend-rx4i.onrender.com"
     const [food_list, setFoodList] = useState([]);
     const [cartItems, setCartItems] = useState({});
     const [token, setToken] = useState("")
